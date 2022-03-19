8f28463fa6571c19aa67ce6f2bdaecc091967e76
Olexiy <a1980dominov@gmail.com>
2022-03-19 16:03:10 +0200

    functional components

--- src/Classcomponent/clcomo.jsx
+++ src/Classcomponent/clcomo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Firstcomp from "./firstcomp";
+import Timer from "../Functioncomponent/funccomponent";
 class Clcomp extends React.Component{
 
     render(){
         return(
             <>
             <Firstcomp/>
+            <hr/>
+            <Timer/>
             </>
         )
     }
---
+++ src/Functioncomponent/funccomponent.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {useState} from 'react';
+
+
+function Timer() {
+const [count, setCount]=useState(0)
+const [text, setText]=useState([])
+
+const myref=React.createRef();
+    let handlercomp=()=>{
+        let increment=count;
+        increment++
+        setCount(increment);
+    }
+    let addtext=()=>{
+       let value1=myref.current.value;
+       let newarr=[...text,value1];
+       setText(newarr);
+       myref.current.value='';
+    }
+    
+    return ( 
+        <>
+        <h2>Timer</h2>
+        <div>
+            <button className='btn btn-danger' onClick={handlercomp}>Start</button>
+        </div>
+        <div>
+            {count}
+        </div>
+        <div>
+            <textarea ref={myref}></textarea>
+        </div>
+        <div>
+            <button className='btn btn-primary' onClick={addtext}>Add text</button>
+        </div>
+        <div>
+            <ul>
+              {text.map((item,index)=><li key={index.toString()}>{item}</li>)}
+            </ul>
+        </div>
+        </>
+     );
+}
+
+export default Timer;
